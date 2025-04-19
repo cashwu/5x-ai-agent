44810af826033b189798e306ab2ec040d2073e07
cash <cashwugeek@icloud.com>
2025-04-19 13:41:57 +0800

    更新 .gitignore，新增 history 目錄；重構 main.py，簡化訊息處理邏輯並修正提示語；新增 message_db.py 檔案以管理訊息存儲

--- main.py
+++ main.py
@@ -2,31 +2,30 @@
 
 from config import OPENAI_API_KEY
 from openai import OpenAI
+from message_db import init_message, add_message, get_messages
 
 client = OpenAI(api_key=OPENAI_API_KEY)
 
+init_message("你是一位專門講關於貓的笑話大師，回答問題時請一律使用**台灣繁體中文**")
+
 try:
     while True:
-        user_question = input("請輸入你的問題：")
+        user_question = input("請輸入你的問題(輸入 exit 可結束對話)：")
 
         if user_question.lower() == "exit":
             print("Bye!")
             break
 
+        add_message(user_question.strip())  # 把 user prompt 存起來
+
         completion = client.chat.completions.create(
             model="gpt-4.1-nano",  # 選擇模型
-            messages=[
-                {
-                    "role": "developer",
-                    "content": "你是一位專門講關於貓的笑話大師，回答問題一律使用**台灣繁體中文**",
-                },
-                {
-                    "role": "user",
-                    "content": user_question,
-                },
-            ],
+            messages=get_messages(),
         )
 
-        print(completion.choices[0].message.content)
+        content = completion.choices[0].message.content
+        add_message(content, role="assistant")  # 把 LLM 的回應也存起來
+
+        print(content)
 except EOFError:
     print("Bye!")
