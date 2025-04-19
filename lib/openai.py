1990d6c72efac769c6bfac93eaa13edc721e986e
cash <cashwugeek@icloud.com>
2025-04-19 16:03:46 +0800

    新增 QdrantDB 類別及 rag.py 檔案，實現向量資料庫的建立與資料寫入功能

--- lib/openai.py
+++ lib/openai.py
@@ -4,3 +4,11 @@ from config import OPENAI_API_KEY
 from openai import OpenAI
 
 client = OpenAI(api_key=OPENAI_API_KEY)
+
+def get_embedding(text):
+    response = client.embeddings.create(
+        model="text-embedding-3-small",
+        input=text,
+    )
+
+    return response.data[0].embedding
