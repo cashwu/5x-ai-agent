3537d8d5579cd52c327873ca697354efbf1b6514
cash <cashwugeek@icloud.com>
2025-04-19 14:54:23 +0800

    更新 function_call.py，新增旋轉加載效果的啟動與停止邏輯

--- function_call.py
+++ function_call.py
@@ -4,6 +4,7 @@ import json
 from message_db import init_message, add_message, get_messages
 from lib.openai import client
 from tools.weather import get_weather
+from utils.spinner import spinner
 
 init_message("你是一位聰明的助理，回答問題的時候請一律使用**台灣繁體中文**")
 add_message("今天台北的天氣如何")  # 為求方便，先固定問題
@@ -30,7 +31,7 @@ tools = [
 ]
 
 AVAILABLE_TOOLS = {"get_weather": get_weather}
-
+spinner.start()
 completion = client.chat.completions.create(
     model="gpt-4.1-nano",  # 選擇模型
     messages=get_messages(),
@@ -38,6 +39,7 @@ completion = client.chat.completions.create(
     tool_choice="auto",
 )
 
+spinner.stop()
 
 completion_message = completion.choices[0].message
 tool_calls = completion_message.tool_calls
