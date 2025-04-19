07bf364ddd222793cc4cb5562a0213768ca57395
cash <cashwugeek@icloud.com>
2025-04-19 11:47:37 +0800

    新增主要功能，實現與 OpenAI 的對話互動

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("hello world")
+# 檔案名稱 main.py
+
+from config import OPENAI_API_KEY
+from openai import OpenAI
+
+client = OpenAI(api_key=OPENAI_API_KEY)
+user_question = input("請輸入你的問題：")
+
+response = client.chat.completions.create(
+    model="gpt-4.1-mini",  # 選擇模型
+    # n=2 # 產生兩個回應
+    messages=[
+        {
+            "role": "developer",
+            "content": "你是一位專門講關於貓的笑話大師，回答問題一律使用**台灣繁體中文**",
+        },
+        {
+            "role": "user",
+            "content": user_question,
+        },
+    ],
+)
+
+
+print(response.choices[0].message.content)
